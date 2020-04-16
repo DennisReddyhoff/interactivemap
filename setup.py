09cccb479d280b644536d8c6021724b0480ab19a
Dennis Reddyhoff <d.reddyhoff@sheffield.ac.uk>
2020-04-16 15:43:48 +0100

    Add setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='kampala_map',
     version='1.0.0',
-    packages= find_packages
+    packages= find_packages,
     zip_safe=False,
     install_requires=[
         'flask',
